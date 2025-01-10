1a581ea3c78a1878bf79f28f1394ea2e8a019662
Erick Lasluisa <erickl1709@gmail.com>
2025-01-10 16:28:15 -0500

    Elimina el servicio y controlador de App, agrega el módulo, controlador y servicio de Provincia, y configura la conexión a la base de datos PostgreSQL.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProvinciaModule } from './provincia/provincia.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    //! Configurar base de datos, usuario, contraseña
+    //TODO Configurar entidades
+    //TODO Configurar variables de entorno
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'votaciones',
+    }),
+    ProvinciaModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
