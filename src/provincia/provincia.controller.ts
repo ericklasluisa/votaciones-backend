b1001501c80711e19f776a9144e1289473c8d6b6
Erick Lasluisa <erickl1709@gmail.com>
2025-01-12 16:30:13 -0500

    Añade funcionalidad para cargar provinicas desde un archivo Excel

--- src/provincia/provincia.controller.ts
+++ src/provincia/provincia.controller.ts
@@ -1,10 +1,53 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseInterceptors,
+  UploadedFile,
+  BadRequestException,
+} from '@nestjs/common';
 import { ProvinciaService } from './provincia.service';
 import { CreateProvinciaDto } from './dto/create-provincia.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller('provincia')
 export class ProvinciaController {
   constructor(private readonly provinciaService: ProvinciaService) {}
+
+  @Post('excel')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: '../../uploads',
+        filename: (req, file, cb) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          cb(null, `${uniqueSuffix}-${file.originalname}`);
+        },
+      }),
+    }),
+  )
+  async uploadExcel(
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<string> {
+    try {
+      if (!file) {
+        throw new BadRequestException('No se ha subido ningún archivo');
+      }
+
+      const filePath = file.path;
+      await this.provinciaService.loadExcelData(filePath);
+
+      return 'Datos cargados correctamente';
+    } catch (error) {
+      console.error(error.message);
+      throw new Error(
+        `Error al procesar las zonas del archivo Excel: ${error.message}`,
+      );
+    }
+  }
 
   @Post()
   create(@Body() createProvinciaDto: CreateProvinciaDto) {
