48926682b1997cc2f1d603c4e51f9d1bc718d203
Erick Lasluisa <erickl1709@gmail.com>
2025-01-28 18:41:27 -0500

    DTO

--- src/dignidad/dto/create-dignidad.dto.ts
+++ src/dignidad/dto/create-dignidad.dto.ts
@@ -1,1 +1,25 @@
-export class CreateDignidadDto {}
+import { Transform } from 'class-transformer';
+import { IsInt, IsOptional, IsString } from 'class-validator';
+import { Canton } from 'src/canton/entities/canton.entity';
+import { Provincia } from 'src/provincia/entities/provincia.entity';
+
+export class CreateDignidadDto {
+  @IsString()
+  nombreDignidad: string;
+
+  @IsOptional()
+  @IsInt()
+  @Transform(({ value }) => parseInt(value))
+  codigoProvincia?: number;
+
+  @IsOptional()
+  @IsInt()
+  @Transform(({ value }) => parseInt(value))
+  codigoCanton?: number;
+
+  @IsOptional()
+  provincia?: Provincia;
+
+  @IsOptional()
+  canton?: Canton;
+}
