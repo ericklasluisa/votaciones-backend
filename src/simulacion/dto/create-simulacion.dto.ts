70b6f568d7cedae2fb7cb22dbc9186eb5db2fb27
Francisco Quiroga <pacoquiroga33@gmail.com>
2025-01-28 20:59:32 -0500

    dto simulacion

--- src/simulacion/dto/create-simulacion.dto.ts
+++ src/simulacion/dto/create-simulacion.dto.ts
@@ -1,8 +1,11 @@
 import { IsDate, IsString } from "class-validator";
+import { Type } from "class-transformer";
 
 export class CreateSimulacionDto {
+    @Type(() => Date) 
     @IsDate()
     fechaCreacion: Date;
+
     @IsString()
     nombreSimulacion: string;
 }
