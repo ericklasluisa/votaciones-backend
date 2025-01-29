e4192ed98281d791a7c09cb607b33921d6ef6cf0
Francisco Quiroga <pacoquiroga33@gmail.com>
2025-01-28 19:07:45 -0500

    Fix Module candidato voto

--- src/voto/voto.module.ts
+++ src/voto/voto.module.ts
@@ -3,9 +3,14 @@ import { VotoService } from './voto.service';
 import { VotoController } from './voto.controller';
 import { Voto } from './entities/voto.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Junta } from 'src/junta/entities/junta.entity';
+import { Simulacion } from 'src/simulacion/entities/simulacion.entity';
+import { Candidato } from 'src/candidato/entities/candidato.entity';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Voto])],
+  imports: [TypeOrmModule.forFeature([Voto, Junta, Simulacion, Candidato]),
+  CommonModule],
   controllers: [VotoController],
   providers: [VotoService],
 })
